46a75144c2d0a6dc85802650686f8a74570c26ef
Raphael Oliveira <rapahaeru@gmail.com>
2020-02-03 16:41:07 -0300

    feat(next-simulator): code improvements

--- pages/quantum.js
+++ pages/quantum.js
@@ -18,17 +18,20 @@ import {
   Input,
   TextArea,
 } from '@catho/quantum';
-import ControlledPagination from '../components/ControlledPagination';
-import ControlledSnackBar from '../components/ControlledSnackBar';
-import SocialsWithProps from '../components/SocialsWithProps';
-import AccordionWithProps from '../components/AccordionWithProps';
-import PopoverTopWithProps from '../components/PopoverTopWithProps';
-import TooltipWithProps from '../components/TooltipWithProps';
-import ControlledModal from '../components/ControlledModal';
-import ControlledCheckboxGroup from '../components/ControlledCheckboxGroup';
-import ControlledDropdown from '../components/ControlledDropdown';
-import RadioGroupWithPros from '../components/RadioGroupWithProps';
-import ToggleWithProps from '../components/ToggleWithProps';
+
+import {
+  ControlledPagination,
+  ControlledSnackBar,
+  SocialsWithProps,
+  AccordionWithProps,
+  PopoverTopWithProps,
+  TooltipWithProps,
+  ControlledModal,
+  ControlledCheckboxGroup,
+  ControlledDropdown,
+  RadioGroupWithPros,
+  ToggleWithProps,
+} from '../components';
 
 import Header from './shared/Header';
 
@@ -46,23 +49,7 @@ return (
           <Header title="Quantum: components simulator" />
         </Col>
       </Row>
-      <Row>
-        <Col {...colsProps}>
-          <Card>
-            <Card.Content>
-              <TabbedView
-                activeTab="Slider">
-                <TabbedView.Tab title="Slider">
-                  <RangeSlider defaultValue={50} />
-                </TabbedView.Tab>
-                <TabbedView.Tab title="Button">
-                  <Button icon="close"> This is a Button</Button>
-                </TabbedView.Tab>
-              </TabbedView>
-            </Card.Content>
-          </Card>
-        </Col>
-      </Row>
+
     </Container>
     <Container>
       <h4>Static components</h4>
@@ -70,19 +57,26 @@ return (
           <Col {...colsProps}>
             <Card>
               <Card.Content>
+                <TabbedView
+                  activeTab="Slider">
+                  <TabbedView.Tab title="Slider">
+                    <RangeSlider defaultValue={50} />
+                  </TabbedView.Tab>
+                  <TabbedView.Tab title="Button">
+                    <Button icon="close"> This is a Button</Button>
+                  </TabbedView.Tab>
+                </TabbedView>
+              </Card.Content>
+              <Card.Content>
                 <h5>Socials</h5>
                 <SocialsWithProps />
               </Card.Content>
-            </Card>
-            <Card>
               <Card.Content>
                 <h5>Hamburger</h5>
                 <Hamburger inverted />
                 <Hamburger inverted showNotification />
                 <Hamburger inverted isOpened />
               </Card.Content>
-            </Card>
-            <Card>
               <Card.Content>
                 <h5>Alert</h5>
                 <Alert icon="info">
@@ -96,14 +90,10 @@ return (
                   </span>
                 </Alert>
               </Card.Content>
-            </Card>
-            <Card>
               <Card.Content>
                 <h5>Badge</h5>
                   <Badge number={10} skin="primary" />This is a badge
               </Card.Content>
-            </Card>
-            <Card>
               <Card.Content>
                 <h5>Tags</h5>
                 <Tag skin="primary">Primary</Tag>
@@ -111,15 +101,11 @@ return (
                 <Tag stroked skin="primary">Primary</Tag>
                 <Tag onClose={() => {}} skin="primary">Primary</Tag>
               </Card.Content>
-            </Card>
-            <Card>
               <Card.Content>
                 <h5>Loaders</h5>
                 <ProgressBar progressPercent={30}  label=" percent processed" />
                 <CircularLoader size="large" />
               </Card.Content>
-            </Card>
-            <Card>
               <Card.Header>
                 <Card.HeaderText>
                 <Card.Title small><Skeleton.Text width="180px" /></Card.Title>
@@ -137,14 +123,10 @@ return (
               <Card.Footer>
                 <Skeleton.Button size="small" width="116px" />
               </Card.Footer>
-            </Card>
-            <Card>
               <Card.Content>
                 <h5>Accordion</h5>
                 <AccordionWithProps />
               </Card.Content>
-            </Card>
-            <Card>
               <Card.Content>
                 <h5>Buttons</h5>
                 <Button icon="close"> This is a Button</Button>
@@ -171,21 +153,15 @@ return (
               <h5>SnackBar</h5>
               <ControlledSnackBar />
             </Card.Content>
-          </Card>
-          <Card>
             <Card.Content>
               <h5>Popover</h5>
               <PopoverTopWithProps />
             </Card.Content>
-          </Card>
-          <Card>
             <Card.Content>
               <h5>Tooltip</h5>
               <TooltipWithProps />
             </Card.Content>
-          </Card>
-          <Card>
-            <Card.Content>
+           <Card.Content>
               <h5>Modal</h5>
               <ControlledModal />
             </Card.Content>
@@ -202,8 +178,6 @@ return (
               <h5>Checkbox</h5>
               <ControlledCheckboxGroup />
             </Card.Content>
-          </Card>
-          <Card>
             <Card.Content>
               <h5>Dropdown</h5>
               <ControlledDropdown />
@@ -214,8 +188,6 @@ return (
                 items={['Lemon', 'Banana', 'Strawberry', 'Orange', 'Avocado']}
               />
             </Card.Content>
-          </Card>
-          <Card>
             <Card.Content>
               <h5>Input</h5>
               <Input
@@ -232,26 +204,19 @@ return (
               <Input label="With an error" error="Error here" />
               <Input label="Disabled" disabled />
             </Card.Content>
-          </Card>
-          <Card>
             <Card.Content>
               <h5>Dropdown</h5>
               <RadioGroupWithPros />
             </Card.Content>
-          </Card>
-          <Card>
             <Card.Content>
               <h5>Toggle</h5>
               <ToggleWithProps />
             </Card.Content>
-          </Card>
-          <Card>
             <Card.Content>
               <h5>TextArea</h5>
               <TextArea helperText="helper text" placeholder="placeholder" label="TextArea label" />
             </Card.Content>
           </Card>
-          
         </Col>
       </Row>
     </Container>
