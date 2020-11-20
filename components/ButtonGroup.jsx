fd1d75d2492129bc281cb039d16b486b1b1bf4b0
Raphael Oliveira <rapahaeru@gmail.com>
2020-11-20 10:33:12 -0300

    updates Quantum and implements buttonGroup example

---
+++ components/ButtonGroup.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { RadioGroup } from "@catho/quantum";
+
+const options = [
+    {
+      "value": "with-icon",
+      "label": "With an Icon",
+      "icon": "info"
+    },
+    {
+      "value": "default",
+      "label": "Default"
+    }
+  ];
+
+const ButtonGroup = () => (
+    <RadioGroup
+    name="groceries"
+    type="button"
+    options={options}
+  />
+);
+
+export default ButtonGroup;
