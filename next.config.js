345d2c971fb18d938aec68e79c1f05437acd2a26
Marcos Conceicao <marcos.conceicao@catho.com>
2022-10-18 11:30:02 -0300

    Rendering all components of catho, quantum and package update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-const path = require('path');
 const withImages = require('next-images');
-
-module.exports = withImages();
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({})
+
+module.exports = withImages()
+module.exports = withBundleAnalyzer({});
--- pages/cathocomponents.js
+++ pages/cathocomponents.js
@@ -1,93 +1,148 @@
-import React from 'react';
-import { Container, Row, Col, Card } from '@catho/quantum';
-import { 
-  HeaderResponsive, 
-  FooterResponsive, 
-  HeaderSimple, 
+import React from "react";
+import {
+  HeaderResponsive,
+  FooterResponsive,
+  HeaderSimple,
   DeprecatedHeaderLoggedResponsive,
   PasswordField,
   HeaderLoggedResponsive,
+  BarChart,
+  CommentsBox,
+  DayPicker,
   ConsentWidget,
- }from '@catho-private/catho-components';
- 
-import Header from './shared/Header';
-import { FooterTagsList, HorizontalMenuDefault, CommentsBoxWithForm } from '../components';
+  FooterSimple,
+  HorizontalMenu,
+  JobCard,
+  JobNotificationList,
+  MessageBox,
+  NotificationIcon,
+  TagsList,
+  DeprecatedConsentWidget,
+} from "@catho-private/catho-components";
 
 export default function CathoComponentsPage() {
   return (
     <>
-      <ConsentWidget apiKey="eZqa0tglwM1TZeiKFpRX9Jekl1cv2iD3iJpeJqV9" />
-      <Container>
-        <Row>
-          <Col>
-            <Header title="catho components: components simulator" />
-          </Col>
-        </Row>
-      </Container>
-      <h4>Header B2C Sem busca</h4>
-      <HeaderResponsive type="B2C" isSubMenuOpen={false} gtmPrefix="next-header-b2c-nosearch" hasSearch />
-      <h4>Header B2C </h4>
-      <HeaderResponsive type="B2C" isSubMenuOpen={false} gtmPrefix="next-header-b2c" />
-      <h4>Header B2B </h4>
-      <HeaderResponsive type="B2B" isSubMenuOpen={false} gtmPrefix="next-header-b2b" loginRedirect={`https://google/com` } hasSearch searchFieldEvents={{onKeyPress: (e) => console.log('keypress: ', e) }}  />
-      <h4>Header Logged B2C (with logged props) </h4>
-      <HeaderResponsive 
-        gtmPrefix="next-header-b2c"       
-        userProfile="STATUS_A_PROFISSIONAL"
-        userThumbnail="https://picsum.photos/24"
-        userMessages={1000}
-        userEvents={3}
-        hasSearch={false}
+      <HeaderResponsive
+        type="B2C"
+        isSubMenuOpen={false}
+        gtmPrefix="next-header-b2c-nosearch"
+        hasSearch
       />
       <HeaderLoggedResponsive userProfile="STATUS_I_PROFISSIONAL" hasSearch />
-      <h4>Header B2B logged</h4>
-      <HeaderLoggedResponsive
-        type="B2B"
-        userProfile="STATUS_LOGGED_HIRER"
-        hasSearch
-        userMessages={9999}
-        userEvents={10}
-      />
-      <h4>[cadastro candidato] Header Simple (implementado pelo área 51)</h4>
       <HeaderSimple gtmPrefix="next" />
-      <h4>[/ajuda] Header Simple logged (implementado pela Transformers)</h4>
-      <DeprecatedHeaderLoggedResponsive gtmPrefix="next" />
-      <h4>[/ajuda] Header Simple logged sem busca (implementado pela Transformers)</h4>
       <DeprecatedHeaderLoggedResponsive gtmPrefix="next" hasSearch={false} />
-      <h4>Footer B2C</h4>
       <FooterResponsive type="B2C" gtmPrefix="next-footer" />
-      <h4>Footer with Tagslist</h4>
-      <FooterTagsList />
-      <Container>
-        <h4>Password Field</h4>
-        <Card>
-          <Card.Content>
-            <PasswordField
-              password={{ label: 'Password'}}
-              onValidate={valid => {
-                console.log('valid', valid);
-              }} />
-          </Card.Content>
-        </Card>
-        
-      </Container>
-      <Container>
-        <h4>Horizontal Menu</h4>
-        <Card>
-          <Card.Content>
-            <HorizontalMenuDefault />
-          </Card.Content>
-        </Card>
-      </Container>
-
-      <Container>
-        <h4>Comments box (with form)</h4>
-        <Card>
-          <Card.Content>
-            <CommentsBoxWithForm />
-          </Card.Content>
-        </Card>
-      </Container>
+      <PasswordField
+        password={{ label: "Password" }}
+        onValidate={(valid) => {
+          console.log("valid", valid);
+        }}
+      />
+      <BarChart
+        data={[
+          {
+            name: "Jan",
+            value: 4000,
+            tooltip: "R$ 4000",
+          },
+        ]}
+      />
+      <CommentsBox><p>Teste</p></CommentsBox>
+      <ConsentWidget apiKey="eZqa0tglwM1TZeiKFpRX9Jekl1cv2iD3iJpeJqV9" />
+      <DayPicker />
+      <FooterSimple />
+      <HorizontalMenu
+        items={[
+          {
+            name: "Buscar Vagas",
+            url: "https://google.com",
+            iconName: "favorite_border",
+            isHighlighted: false,
+          },
+        ]}
+      />
+      <JobCard
+        jobID="123"
+        jobURL="https://www.catho.com.br"
+        name="Test"
+        company={{
+          name: "Catho",
+          avatar: undefined,
+          isConfidential: false,
+          stamps: [
+            {
+              icon: "person",
+              description: "",
+            },
+          ],
+        }}
+        salaryRange="test"
+        benefits={[
+          {
+            name: "Alimentação",
+            icon: "receipt",
+          },
+        ]}
+        publishDate={new Date(2022, 4, 10, 13).getTime()}
+        updateDate={new Date(2022, 5, 10, 13).getTime()}
+        locations={[
+          {
+            name: "test",
+            quantity: 15,
+            url: "https://www.catho.com.br",
+          },
+        ]}
+        quantityTotal={10}
+        description="test"
+        isPCD={false}
+        isExtended={false}
+        isCompatible={false}
+        isFavorite={false}
+        cta={{
+          type: "DEFAULT",
+          dateSent: new Date(2022, 6, 10, 13).getTime(),
+          timeToUmblock: new Date(2022, 6, 10, 14).getTime(),
+          subscribeLink: "https://www.catho.com.br",
+        }}
+      />
+      <JobNotificationList
+        data={[
+          {
+            id: "123",
+            title: "Designer de Produtos, UX Design",
+            qtde: 290,
+            city: [
+              {
+                name: "São Paulo",
+                uf: "SP",
+              },
+            ],
+            state: [],
+            url: "https://www.catho.com.br/vagas/ux-designer/?q=ux%20designer",
+          },
+        ]}
+      />
+      <MessageBox />
+      <NotificationIcon type="folder" />
+      <TagsList
+        contents={[
+          {
+            label: "Candidates",
+            items: [
+              {
+                url: "http://google.com",
+                label: "Vagas de home office",
+                dataGtmEventName: "interaction:normal-metric",
+                dataGtmEventCategory: "catho",
+                dataGtmEventLabel: "link:zhaopin",
+                dataGtmEventAction: "novo-footer:parceiros-internacionais",
+              },
+            ],
+          },
+        ]}
+      />
+      <DeprecatedConsentWidget apiKey="eZqa0tglwM1TZeiKFpRX9Jekl1cv2iD3iJpeJqV9"/>
     </>
   );
 }
