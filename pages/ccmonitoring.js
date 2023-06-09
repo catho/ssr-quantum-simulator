9be41fb38c384f98aaed04eec2f3b8197d4a4700
Raphael Oliveira <rapahaeru@gmail.com>
2023-06-09 17:26:46 -0300

    update instructions

--- pages/ccmonitoring.js
+++ pages/ccmonitoring.js
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "@catho/quantum/Grid/sub-components/Container";
 import Col from "@catho/quantum/Grid/sub-components/Col";
 import Row from "@catho/quantum/Grid/sub-components/Row";
-import Head from "next/head"; 
 import HeaderResponsive from "@catho-private/catho-components/HeaderResponsive";
 
 const colsProps = {
@@ -22,9 +21,12 @@ export default () => (
     <Row>
         <Col {...colsProps}>
           <h3>Catho components monitoring page</h3>
-          <p>Here the instructions to collect the payload in Browser and the bundle size</p>
+          <p>Here the instructions to collect the payload in Browser and the bundle size:</p>
+          
           <ol>
-            <li> You should be here via task runner <code>yarn analyzer</code></li>
+            <li>You should be here via task runner <code>yarn analyzer</code></li>
+            <li>the libs version must be inserted without "^" signal in package.json. We must test the real version of the package</li>
+            <li>keep the yarn lock from master</li>
             <li>find the <i>ccmonitoring</i> chunk at the client page of analyzer</li>
             <li>collect the bundle size by the <strong>"Parsed size"</strong> item </li>
             <li>collect the lib payload in browser by the <strong>"Gziped size"</strong> item </li>
