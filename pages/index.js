24e45242b0139d1922cbf341d46d52c751f4fc4b
Raphael Oliveira <rapahaeru@gmail.com>
2023-06-12 17:42:58 -0300

    add instructions and new task runner to collect metrics

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,24 @@ import Container from "@catho/quantum/Grid/sub-components/Container";
 import Col from "@catho/quantum/Grid/sub-components/Col";
 import Row from "@catho/quantum/Grid/sub-components/Row";
 import Link from "./styles/style";
+import Alert from "@catho/quantum/Alert"
+import List from "@catho/quantum/List"
 import Head from "next/head"; 
 
 const colsProps = {
   medium: 12,
 };
+
+const itemsList = [
+  { content: 'You should be here via task runner yarn analyzer' },
+  { content: 'the libs version must be inserted without "^" signal in package.json. We must test the real version of the package' },
+  { content: 'choose the page do you want to collect, Quantum or Catho Components' },
+  { content: 'pages will open, find the corresponding library in its chunk (with same name)' },
+  { content: 'reloads the page do you want to collect' },
+  { content: 'point the mouse in node_modules of the page (quantum or cathocomponents)' },
+  { content: 'collect the bundle size by the "Parsed size" item ' },
+  { content: 'collect the lib payload in browser by the "Gziped size" item ' },
+]
 
 export default () => (
   <>
@@ -16,21 +29,29 @@ export default () => (
         <Col {...colsProps}>
           <h3>To monitoring page ...</h3>
           <p>Here the instructions to collect the payload in Browser and the bundle size:</p>
-
-          <ol>
-            <li>You should be here via task runner <code>yarn analyzer</code></li>
-            <li>the libs version must be inserted without "^" signal in package.json. We must test the real version of the package</li>
-            <li>keep the yarn lock from master</li>
-            <li>choose the page do you want to collect, Quantum or Catho Components</li>
-            <li>pages will open, find the corresponding library in its chunk (with same name)</li>
-            <li>reloads the page do you want to collect</li>
-            <li>point the mouse in node_modules of the page (quantum or cathocomponents)</li>
-            <li>collect the bundle size by the <strong>"Parsed size"</strong> item </li>
-            <li>collect the lib payload in browser by the <strong>"Gziped size"</strong> item </li>
-          </ol>
+          <List items={itemsList} ordered />
         </Col>
     </Row>
     </Container>
+    <Container>
+    <Row>
+        <Col {...colsProps}>
+          <Alert
+            icon="info"
+            skin="primary"
+          >
+            <span>
+              <strong>Important:</strong> this bundle is bigger than applications uses. Here, we use all the components that 
+              library offers, and some duplicated ones.
+              One application do not use 2 headers or all components.
+              Its only our unit of measurement.
+            </span>
+          </Alert>
+        </Col>
+    </Row>
+    </Container>
+
+
 
     <Container>
       <Head>
