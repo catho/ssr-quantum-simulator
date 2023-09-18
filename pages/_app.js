7cfd76c56a4436f9f5fc4342c65142193a857b6b
alizeleal <alize.de.fatima@gmail.com>
2023-09-18 10:25:10 -0300

    fixes favicon and code location

--- pages/_app.js
+++ pages/_app.js
@@ -42,11 +42,6 @@ export default class HomePage extends App {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <link
-            rel="shortcut icon"
-            type="image/x-icon"
-            href="../static/imgs/favicon.ico"
-          />
         </Head>
         <GlobalStyle />
         <header>
