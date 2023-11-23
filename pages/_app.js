63cdfedf78ba9688096e69e86a8a5178fd6ed20c
alizeleal <alize.de.fatima@gmail.com>
2023-11-23 11:55:10 -0300

    removes JS and uses the checkbox hack on hidden menu

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,10 @@
 import App from 'next/app';
 import Head from 'next/head';
 import GlobalStyle from '@catho/quantum/GlobalStyle';
-import { Container, Col, Row } from '@catho/quantum';
+import { Container } from '@catho/quantum';
 import ScrollUpButton from '../components/ScrollUpButton';
 
 export default class HomePage extends App {
-  // componentDidMount() {
-  //   const hiddenMenu = document.getElementById('hiddenDesktopMenu');
-
-  //   const menuButton = document.getElementById('menuSandwichButton');
-
-  //   menuButton.addEventListener('click', showMenu);
-  //   function showMenu() {
-  //     const showHiddenMenu = window.innerWidth > 1023;
-  //     const showMobileMenu = window.innerWidth < 1024;
-  //     if (hiddenMenu.classList.contains('HideMenu') && showHiddenMenu) {
-  //       hiddenMenu.classList.add('ShowMenu');
-  //       hiddenMenu.classList.remove('HideMenu');
-  //     } else {
-  //       hiddenMenu.classList.remove('ShowMenu');
-  //       hiddenMenu.classList.add('HideMenu');
-  //     }
-
-  //     if (hiddenMenu.classList.contains('MobileMenu') && showMobileMenu) {
-  //       hiddenMenu.classList.add('HideMenu');
-  //       hiddenMenu.classList.remove('MobileMenu');
-  //     } else if (hiddenMenu.classList.contains('HideMenu') && showMobileMenu) {
-  //       hiddenMenu.classList.remove('HideMenu');
-  //       hiddenMenu.classList.add('MobileMenu');
-  //     }
-
-  //     if (showHiddenMenu) {
-  //       hiddenMenu.classList.remove('MobileMenu');
-  //     }
-  //   }
-  // }
-
   render() {
     const { Component, pageProps } = this.props;
 
@@ -51,79 +20,55 @@ export default class HomePage extends App {
         <GlobalStyle />
         <header>
           <Container>
-            <Row>
-              <Col
-                medium={4}
-                small={4}
-                xsmall={2}
-                large={4}
-                className="CathoLogo"
+            <div className="NavigationBar">
+              <a href="/">
+                <img src="../static/imgs/cathoLogo.png" alt="Logotipo Catho" />
+              </a>
+              <nav
+                aria-label="Menu Principal"
+                id="visibleDesktopMenu"
+                className="VisibleMenu  FlexEndPositioning"
               >
-                <a href="/">
-                  <img
-                    src="../static/imgs/cathoLogo.png"
-                    alt="Logotipo Catho"
-                  />
-                </a>
-              </Col>
-              <Col medium={8} small={4} xsmall={2} large={8}>
-                <nav
-                  aria-label="Menu Principal"
-                  id="visibleDesktopMenu"
-                  className="VisibleMenu  FlexEndPositioning"
-                >
-                  <ul role="menu" className="NavList">
-                    <li role="menuitem" className="DisplayNoneOnSmallScreen">
-                      <a href="/cathocomponents">Catho Components</a>
-                    </li>
-                    <li role="menuitem" className="DisplayNoneOnSmallScreen">
-                      <a href="/quantum">Quantum</a>
-                    </li>
-                    <li>
-                      <input
-                        type="checkbox"
-                        aria-controls="hiddenDesktopMenu"
-                        aria-expanded="false"
-                        aria-label="Menu"
-                        id="menuSandwichButton"
-                      />
-                      <label
-                        className="SandwichButton"
-                        htmlFor="menuSandwichButton"
-                      >
-                        &#x2630;
-                      </label>
-                      <nav
-                        aria-label="Menu de opções"
-                        id="hiddenDesktopMenu"
-                        className="HiddenMenu  "
-                      >
-                        <ul role="menu">
-                          <li
-                            role="menuitem"
-                            className="HiddenMenuNavigationItem"
-                          >
-                            <a href="/cathocomponents">Catho Components</a>
-                          </li>
-                          <li
-                            role="menuitem"
-                            className="HiddenMenuNavigationItem"
-                          >
-                            <a href="/quantum">Quantum</a>
-                          </li>
-                          <li role="menuitem">
-                            <a href="/render">Simulates renders</a>
-                          </li>
-                          <li role="menuitem">
-                            <a href="https://github.com/catho">Repositories</a>
-                          </li>
-                        </ul>
-                      </nav>
-                    </li>
-                  </ul>
-                </nav>
-              </Col>
-            </Row>
+                <ul role="menu" className="NavList">
+                  <li role="menuitem" className="DisplayNoneOnSmallScreen">
+                    <a href="/cathocomponents">Catho Components</a>
+                  </li>
+                  <li role="menuitem" className="DisplayNoneOnSmallScreen">
+                    <a href="/quantum">Quantum</a>
+                  </li>
+                </ul>
+              </nav>
+              <input
+                type="checkbox"
+                aria-controls="hiddenDesktopMenu"
+                aria-expanded="false"
+                aria-label="Menu"
+                id="menuSandwichButton"
+              />
+              <label className="SandwichButton" htmlFor="menuSandwichButton">
+                &#x2630;
+              </label>
+              <nav
+                aria-label="Menu de opções"
+                id="hiddenDesktopMenu"
+                className="HiddenMenu  "
+              >
+                <ul role="menu">
+                  <li role="menuitem" className="HiddenMenuNavigationItem">
+                    <a href="/cathocomponents">Catho Components</a>
+                  </li>
+                  <li role="menuitem" className="HiddenMenuNavigationItem">
+                    <a href="/quantum">Quantum</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="/render">Simulates renders</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="https://github.com/catho">Repositories</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </Container>
         </header>
         <Container className="MainContent WhiteBackground">
