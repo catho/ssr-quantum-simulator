0d832f16daf494deb3a9bcc3d83f963398113cf0
alizeleal <alize.de.fatima@gmail.com>
2024-01-26 16:23:24 -0300

    includes unit test for logo's link

--- components/Header/Header.unit.test.jsx
+++ components/Header/Header.unit.test.jsx
@@ -11,10 +11,13 @@ test('should render logo and NavigationMenu', () => {
   render(<Header />);
 
   const logo = screen.getByRole('img');
+  const pageLinks = screen.getAllByRole('link');
+  const homeLink = pageLinks[0];
   const navigationMenu = screen.getByRole('navigation', {
     name: 'Menu Principal',
   });
 
   expect(logo).toBeInTheDocument();
+  expect(homeLink).toBeInTheDocument();
   expect(navigationMenu).toBeInTheDocument();
 });
